4475ffac38dee1f2aaaadd61feabf46a0157e7e4
Gareth Ferguson <gareth.f@hotmail.co.uk>
2021-09-10 11:36:58 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,134 @@
+module.exports = {
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+    ],
+    plugins: [
+        "@typescript-eslint",
+        "prefer-arrow",
+        "prettier",
+        "simple-import-sort",
+        "react",
+        "react-hooks",
+    ],
+    env: {
+        es6: true,
+        browser: true,
+    },
+    settings: {
+        "import/parsers": {
+            "@typescript-eslint/parser": [".ts", ".tsx"],
+        },
+        "import/resolver": {
+            typescript: {
+                alwaysTryTypes: true,
+            },
+            node: {
+                extensions: [".js", ".jsx", ".ts", ".tsx"],
+            },
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/camelcase": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-explicit-any": "error",
+        "@typescript-eslint/member-delimiter-style": [
+            "off",
+            {
+                multiline: {
+                    delimiter: "semi",
+                    requireLast: true,
+                },
+                singleline: {
+                    delimiter: "semi",
+                    requireLast: false,
+                },
+            },
+        ],
+        "comma-dangle": "off",
+        curly: "error",
+        "dot-notation": "off",
+        "eol-last": "error",
+        eqeqeq: ["error", "smart"],
+        "guard-for-in": "error",
+        "id-blacklist": [
+            "error",
+            "Number",
+            "number",
+            "String",
+            "string",
+            "Boolean",
+            "boolean",
+            "Undefined",
+            "undefined",
+        ],
+        "id-match": "error",
+        "no-bitwise": "error",
+        "no-caller": "error",
+        "no-cond-assign": "error",
+        "no-console": "warn",
+        "no-debugger": "error",
+        "no-empty": "error",
+        "no-eval": "error",
+        "no-fallthrough": "error",
+        "no-multiple-empty-lines": "off",
+        "no-new-wrappers": "error",
+        "no-redeclare": "error",
+        "no-shadow": [
+            "error",
+            {
+                hoist: "all",
+            },
+        ],
+        "no-trailing-spaces": "error",
+        "no-underscore-dangle": "error",
+        "no-unused-expressions": "error",
+        "no-unused-labels": "error",
+        "no-unsafe-finally": "off",
+        "no-var": "error",
+
+        "prefer-arrow/prefer-arrow-functions": [
+            "warn",
+            {
+                disallowPrototype: true,
+                singleReturnOnly: true,
+                classPropertiesAllowed: true,
+            },
+        ],
+        "quote-props": ["error", "as-needed"],
+        radix: "error",
+        "spaced-comment": "error",
+        "space-before-function-paren": [
+            "error",
+            {
+                anonymous: "never",
+                asyncArrow: "always",
+                named: "never",
+            },
+        ],
+        "use-isnan": "error",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            {
+                vars: "all",
+                args: "after-used",
+                ignoreRestSiblings: true,
+                argsIgnorePattern: "^_",
+                varsIgnorePattern: "^_",
+            },
+        ],
+        "react/react-in-jsx-scope": "off",
+        "value-keyword-case": "off",
+        "simple-import-sort/imports": "error",
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+}
 
